4f1b03c6e0307d5fd3f94f8ee1478d12f204da85
Raul Stepan <raulstepan97@gmail.com>
2020-01-28 17:59:36 +0200

    add mnemonic phrase function

--- mnemonic/mnemonic_factory.py
+++ mnemonic/mnemonic_factory.py
@@ -14,7 +14,7 @@ def fill_bits(binary, bits):
     return binary
 
 
-# generate a given number of entropy bits
+# generate a given number of entropy bits using CSPRNG from /dev/urandom
 def generate_entropy(bits=256):
     if bits < 128 or bits > 256:
         raise EntropyRangeExceeded
@@ -30,8 +30,25 @@ def sha256(_input):
 
 # returns the checksum of the input hash
 # checksum is given by the first (entropy length / 32)
-# characters of the sha256 hash applied on entropy bits
+# bits of the sha256 hash applied on entropy bits
 def get_checksum(_entropy):
     entropy_length = len(_entropy) // 32
-    return sha256(_entropy)[:entropy_length]
+    return bin(int(sha256(_entropy), 16))[2:][:entropy_length]
 
+
+# separate the entropy+checksum bits in chunks of 11 bits
+# and map them to the word list to get english words
+# prints and returns the mnemonic phrase
+def get_mnemonics(entropy):
+    checksum = get_checksum(entropy)
+    entcs = entropy + checksum[:len(entropy) // 32]
+    bitlist = [entcs[i:i+11] for i in range(0, len(entcs), 11)]
+    wordlist = []
+    with open("wordlist", 'r') as wl:
+        wlines = wl.readlines()
+        for i in bitlist:
+            wordlist.append(wlines[int(i, 2)].split("\n")[0])
+
+    wordlist = " ".join(wordlist)
+    print(wordlist)
+    return wordlist
