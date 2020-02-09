a88ce32e1f9aaedaeedda8be518e2ce82bce506d
Raul Stepan <raulstepan97@gmail.com>
2020-02-09 18:20:25 +0200

    add main() exec

---
+++ main.py
@@ -0,0 +1,12 @@
+from mnemonic import mnemonic_factory
+
+
+entropy = mnemonic_factory.generate_entropy(128)
+mnemonic = mnemonic_factory.get_mnemonics(entropy)
+seed = mnemonic_factory.get_seed(mnemonic, "test", binary=True)
+
+print("[+] Entropy: {}".format(entropy))
+print("[+] Menmonic: {}".format(mnemonic))
+print("[+] Seed: {}".format(seed))
+print("[!] Seed length: {}".format(len(seed)))
+
