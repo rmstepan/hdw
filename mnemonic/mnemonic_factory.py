d66f2cc2da2bb314ff5e197fc83a5ef482528373
rmstepan <raulstepan97@gmail.com>
2020-05-09 15:38:18 +0300

    fix sha256 for checksum; change byteorder to big

--- mnemonic/mnemonic_factory.py
+++ mnemonic/mnemonic_factory.py
@@ -19,13 +19,14 @@ def generate_entropy(bits=256):
     if bits < 128 or bits > 256:
         raise EntropyRangeExceeded
 
-    entropybits = bin(int.from_bytes(os.urandom(bits // 8), byteorder=sys.byteorder))[2:]
-    return fill_bits(entropybits, bits)
+    entropy_bits = bin(int.from_bytes(os.urandom(bits // 8), byteorder="big"))[2:]
+    return fill_bits(entropy_bits, bits)
 
 
 # returns the sha256 hash of the given input
 def sha256(_input):
-    return hashlib.sha256(_input.encode("utf-8")).hexdigest()
+    ent_bytes = int(_input, 2).to_bytes(len(_input) // 8, byteorder='big')
+    return hashlib.sha256(ent_bytes).hexdigest()
 
 
 # returns the checksum of the input hash
@@ -33,7 +34,7 @@ def sha256(_input):
 # bits of the sha256 hash applied on entropy bits
 def get_checksum(_entropy):
     entropy_length = len(_entropy) // 32
-    return bin(int(sha256(_entropy), 16))[2:][:entropy_length]
+    return bin(int(sha256(_entropy), 16))[2:].zfill(256)[:entropy_length]
 
 
 # separate the entropy+checksum bits in chunks of 11 bits
@@ -45,6 +46,9 @@ def get_mnemonics(entropy):
     entcs = entropy + checksum[:len(entropy) // 32]
     # separate the entropy+checksum into chunks of 11 bits
     bitchunks = [entcs[i:i+11] for i in range(0, len(entcs), 11)]
+    print("Entropy: {}".format(entropy))
+    print("Entcs:   {}".format(entcs))
+    print(bitchunks)
     wordlist = []
     with open("mnemonic/wordlist", 'r') as wl:
         wlines = wl.readlines()
@@ -81,5 +85,5 @@ def get_seed(mnemonic, passphrase=None, binary=False):
     _seed = hashlib.pbkdf2_hmac("sha512", bytes(mnemonic, 'utf-8'),
                                 bytes(passphrase, 'utf-8'), 2048)
 
-    return fill_bits(bin(int.from_bytes(_seed, byteorder=sys.byteorder))[2:], 512) if binary else \
-        hex(int.from_bytes(_seed, byteorder=sys.byteorder))[2:]
+    return fill_bits(bin(int.from_bytes(_seed, byteorder="big"))[2:], 512) if binary else \
+        hex(int.from_bytes(_seed, byteorder="big"))[2:]
