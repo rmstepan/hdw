6ace9ecead2fe96d1470749cee85fade16a82e2b
rmstepan <raulstepan97@gmail.com>
2020-07-14 17:14:59 +0300

    add key derivation functions

--- mnemonic/mnemonic_factory.py
+++ mnemonic/mnemonic_factory.py
@@ -1,6 +1,9 @@
 import os
 import sys
+import hmac
 import hashlib
+import ecdsa
+from ecdsa.ecdsa import generator_secp256k1
 
 
 class EntropyRangeExceeded(Exception):
@@ -133,3 +136,138 @@ def get_seed(mnemonic, passphrase=None, binary=False):
 
     return fill_bits(bin(int.from_bytes(_seed, byteorder="big"))[2:], 512) if binary else \
         hex(int.from_bytes(_seed, byteorder="big"))[2:]
+
+
+##
+# get the master private key, public key and master chain code from the master seed
+# _seed -> master seed generated from mnemonic (64 bytes); hex or bin
+# return: (xpriv, xpub), (priv, pub, chain_code)
+def get_master_code(_seed):
+    if is_binary(_seed):
+        _seed = int(_seed, 2).to_bytes(len(_seed) // 8, byteorder='big')
+    else:
+        _seed = bytes.fromhex(_seed)
+
+    seed = hmac.new(b"Bitcoin seed", _seed, digestmod=hashlib.sha512).digest()
+
+    # add formatting bytes
+    xprv = MAINNET_XPRV  # version for mainnet private key
+    xprv += b"\x00" * 9  # Depth(1b), child number(4b), parent fingerprint(4b)
+
+    il = seed[:32]  # master private key
+    ir = seed[32:]  # master chain code
+
+    xprv += ir
+    xprv += b"\x00" + il
+
+    # Double hash using SHA256
+    hashed_xprv = hashlib.sha256(xprv).digest()
+    hashed_xprv = hashlib.sha256(hashed_xprv).digest()
+
+    # Append 4 bytes of checksum
+    xprv += hashed_xprv[:4]
+
+    ##
+    # create master public key
+    privkey_obj = ecdsa.SigningKey.from_string(il, curve=ecdsa.SECP256k1)
+    pubkey_obj = privkey_obj.get_verifying_key().to_string("compressed")
+
+    xpub = MAINNET_XPUB  # version for mainnet public key
+    xpub += b"\x00" * 9
+    xpub += ir
+    xpub += pubkey_obj
+
+    # Double hash using SHA256
+    hashed_xpub = hashlib.sha256(xpub).digest()
+    hashed_xpub = hashlib.sha256(hashed_xpub).digest()
+
+    # add 4 checksum bytes
+    xpub += hashed_xpub[:4]
+
+    # return base 58 tuple (master code, (master private key, master chain code))
+
+    return (b58encode(xprv), b58encode(xpub)), (il, pubkey_obj, ir)
+
+
+##
+# derive a new child from the given parent
+# parent_priv -> Parent private key (32 bytes)
+# parent_pub  -> Parent public key (33 bytes - compressed)
+# hardened    -> 1 for hardened derivation or 0 for normal derivation
+# depth       -> the child depth in the tree
+# index       -> the child index to be derived
+def derive_child(parent_priv, parent_pub, parent_chain_code, hardened=False, depth=0, index=0):
+    if index < 0:
+        raise ValueError("Child index should be greater than or equal to 0.")
+    if depth < 0:
+        raise ValueError("Depth should be greater than or equal to 0.")
+
+    if not hardened:
+        # concatenate parent public key || child index -> normal derivation
+        parent_index = parent_pub + int.to_bytes(index, length=4, byteorder="big")
+    else:
+        # concatenate parent private key || child index -> hardened derivation
+        parent_index = b"\x00" + parent_priv + int.to_bytes(index, length=4, byteorder="big")
+
+    # HMAC that shit
+    data = hmac.new(parent_chain_code, parent_index, digestmod=hashlib.sha512).digest()
+
+    # set child chain code to IR
+    child_chain_code = data[32:]
+    il = data[:32]
+
+    # build child private key -> parent private key + IL (scalar addition) (mod n)
+    child_private_key = int.from_bytes(parent_priv, byteorder="big") + int.from_bytes(il, byteorder="big")
+    child_private_key = child_private_key % generator_secp256k1.order()
+    child_private_key = int.to_bytes(child_private_key, length=32, byteorder="big")
+
+    # build child public key from SECP256k1 curve
+    # compress the pubkey to 33 bytes
+    # y point is discarded and replaced with 0x02 for evenness or 0x03 for oddness -> the header byte
+    privkey_obj = ecdsa.SigningKey.from_string(child_private_key, curve=ecdsa.SECP256k1)
+    pubkey_obj = privkey_obj.get_verifying_key().to_string("compressed")
+    child_public_key = pubkey_obj
+
+    #####################################################################################
+    # Extend child private key
+    extended_priv = MAINNET_XPRV                             # add version - 4 bytes
+    extended_priv += int.to_bytes(depth, length=1, byteorder="big")  # add depth - 1 byte
+    extended_priv += int.to_bytes(index, length=4, byteorder="big")  # add index - 4 bytes
+
+    # add first 4 bytes (parent fingerprint) from hash160 hash (ripemd160(sha256(xpriv)))
+    sha = hashlib.sha256(b"\x00" + parent_priv).digest()
+    hash160 = hashlib.new("ripemd160")
+    hash160.update(sha)
+    hash160 = hash160.digest()
+
+    extended_priv += hash160[:4]
+    extended_priv += child_chain_code
+    extended_priv += b"\x00" + child_private_key
+
+    hashed_xpriv = hashlib.sha256(extended_priv).digest()
+    hashed_xpriv = hashlib.sha256(hashed_xpriv).digest()
+
+    extended_priv += hashed_xpriv[:4]
+
+    #######################################################################################
+    # Extend child public key
+    extended_pub = MAINNET_XPUB     # add version - 4 bytes
+    extended_pub += int.to_bytes(depth, length=1, byteorder="big")  # add depth - 1 byte
+    extended_pub += int.to_bytes(index, length=4, byteorder="big")  # add index - 4 bytes
+
+    # add first 4 bytes (parent fingerprint) from hash160 hash (ripemd160(sha256(xpub)))
+    sha = hashlib.sha256(parent_pub).digest()
+    hash160 = hashlib.new("ripemd160")
+    hash160.update(sha)
+    hash160 = hash160.digest()
+
+    extended_pub += hash160[:4]
+    extended_pub += child_chain_code
+    extended_pub += child_public_key
+
+    hashed_xpub = hashlib.sha256(extended_pub).digest()
+    hashed_xpub = hashlib.sha256(hashed_xpub).digest()
+
+    extended_pub += hashed_xpub[:4]
+
+    return (b58encode(extended_priv), b58encode(extended_pub)), (child_private_key, child_public_key, child_chain_code)
